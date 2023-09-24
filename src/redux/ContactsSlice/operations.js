32ec3b3b2e840ffc6dfaad6d01fb8184c5951f43
Yaroslav337 <pelerovteatr@gmail.com>
2023-09-24 03:49:05 +0300

    fix ContactsSlice/operations.js

--- src/redux/ContactsSlice/operations.js
+++ src/redux/ContactsSlice/operations.js
@@ -5,7 +5,7 @@ export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('/gcontacts');
+      const response = await axios.get('/contacts');
 
       return response.data;
     } catch {
